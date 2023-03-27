a5266be032d98082ba3d202f00dcff865b7557b6
hinacarlo <hinacarlo1993@gmail.com>
2023-03-27 14:21:22 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+
+const notFound = require("./middleware/404");
+const errorHandler = require("./middleware/error-handler");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+const startServer = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
